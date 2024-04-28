b66dac09bce8bb47f4ce46c29f12e8fa3b92a58e
Eric Cartman <kelemena@kkszki.hu>
2024-04-28 17:07:25 +0200

    Suspicious window fixed, Suspicious user class fixed

--- Connect2GetherWPF/Models/SuspiciousUser.cs
+++ Connect2GetherWPF/Models/SuspiciousUser.cs
@@ -11,14 +11,15 @@ namespace Connect2GetherWPF.Models
         public int id {  get; set; }
         public int userId { get; set; }
         public User user { get; set; }
-        public string username { get; set; }
+        public string username { get { return user.username; } set { 
+            username = user.username;
+            } }
 
         public SuspiciousUser(int id, int userId, User user)
         {
             this.id = id;
             this.userId = userId;
             this.user = user;
-            username = user.username;
         }
     }
 }
